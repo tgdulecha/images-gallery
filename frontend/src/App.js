0ee2377741b301b27f92953b9dc61580bdcb3a5a
tinsae <tinsae.dulecha@univr.it>
2021-07-21 18:29:51 +0200

    controlled search component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Header from './components/Header';
-function App() {
+import Search from './components/Search';
+import { useState } from 'react'
+const App= ()=> {
+  const [word, setWord] = useState('')
+  const handleSearchSubmit = (e)=>{
+    e.preventDefault();
+    console.log(word);
+  }
   return (
     <div className="App">
       <Header title = "Image Gallery"/>
+      <Search word={word} setWord={setWord} handleSubmit={handleSearchSubmit}/>
     </div>
   );
 }
--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,17 @@
 import React from 'react'
-import {Navbar} from 'react-bootstrap' 
+import { Navbar, Container } from 'react-bootstrap'
+const navbarStyle = {
+    backgroundColor: 'lightblue'
 
-const Header = ({title})=>{
+}
+const Header = ({ title }) => {
     return (
-        <Navbar bg="light" variant="light">
-<Navbar.Brand href="/">{title}</Navbar.Brand>
+        <Navbar style={navbarStyle} variant="light">
+            <Container>
+                <Navbar.Brand href="/">{title}</Navbar.Brand>
+            </Container>
 
-</Navbar>
+        </Navbar>
 
     )
 }
