e8a6f0f83ff717a6b769c372dbd35c45e74bb6d3
tinsae <tinsae.dulecha@univr.it>
2021-07-21 16:48:12 +0200

    intial header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,9 @@
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Header from './components/Header';
 function App() {
   return (
     <div className="App">
-      <h1>Images Gallery </h1>
+      <Header title = "Image Gallery"/>
     </div>
   );
 }
